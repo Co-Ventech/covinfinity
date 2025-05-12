d8ffad4374f07253ad23da6947e83a1de34346ed
Eloquence98 <ibneenayat1998@gmail.com>
2025-05-12 20:30:05 +0500

    fix: add gradiant second color start percent

--- app/components/CompanySlider.tsx
+++ app/components/CompanySlider.tsx
@@ -69,10 +69,13 @@ interface LogoItemProps {
 
 function LogoItem({ children, icon = false }: LogoItemProps) {
   return (
-    <div className="relative mx-4 inline-block p-[1px] rounded-[1.25rem]" style={{
-      background: 'linear-gradient(180deg, rgba(31, 34, 36, 1) 100%, rgba(22, 24, 26, 1) 100%)'
-    }}>
-      <div className="relative flex items-center rounded-[1.2rem] bg-gradient-to-b from-[#16181A] to-[#0F1011] px-8 py-4 text-[1.2rem] font-semibold text-[#F0F2FF]">
+    <div
+      className="relative mx-4 inline-block rounded-[1.25rem] p-[1px]"
+      style={{
+        background: 'linear-gradient(180deg, rgba(31, 34, 36, 1) 100%, rgba(22, 24, 26, 1) 100%)',
+      }}
+    >
+      <div className="relative flex items-center rounded-[1.2rem] bg-gradient-to-b from-[#16181A] to-[#0F1011] to-70% px-8 py-4 text-[1.2rem] font-semibold text-[#F0F2FF]">
         {icon && (
           <div className="mr-2 flex h-5 w-5 items-center justify-center rounded-sm bg-white">
             <div className="h-3 w-3 bg-black"></div>
