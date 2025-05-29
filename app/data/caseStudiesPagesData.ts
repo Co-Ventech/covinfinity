fe9c7484d2dbdb58db2080f902b5fa5b6fc2dbdb
eloquence98 <ibneenayat1998@gmail.com>
2025-05-29 17:00:16 +0500

    chore: remove unused PRODUCTS data from caseStudiesPagesData.ts

--- app/data/caseStudiesPagesData.ts
+++ app/data/caseStudiesPagesData.ts
@@ -82,16 +82,3 @@ export const CASE_STUDIES = [
       link: 'https://creditbook.com'
     }
   ];
-
-
-export const PRODUCTS = [
-    {
-      name: 'Recruitinn',
-      slug: 'recruitinn',
-      link: 'https://app.recruitinn.ai',
-      heading: `Recruitinn Heading Title Can be\nAnything XYZ upto You`,
-      subheading:
-        'Some body text goes here can add whatever you want as body text maybe a description and for whom this project is for idk it’s just a placeholder',
-    },
-  ];
-  
